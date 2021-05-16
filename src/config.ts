2db6c83c9afbe45fa8e4ab6857af0ec1ee5025ae
Manuel Römer <30902964+manuelroemer@users.noreply.github.com>
2021-05-16 13:00:24 +0200

    Support local env files using dotenv.

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,14 @@
+import { logger } from './log';
+
+// Dev builds can leverage a local .env file for custom settings.
+if (process.env.NODE_ENV !== 'production') {
+  // eslint-disable-next-line @typescript-eslint/no-var-requires
+  const dotenv = require('dotenv');
+  dotenv.config();
+  logger.info('Using development configuration values from the .env file.');
+}
+
 export const config = {
-  env: process.env.NODE_ENV ?? 'production',
-  port: 4040,
-  dbConnectionString: 'CONNECTION_STRING',
+  port: process.env.PORT ?? 4040,
+  dbConnectionString: process.env.MONGODB_CONNECTION_STRING ?? '',
 };
