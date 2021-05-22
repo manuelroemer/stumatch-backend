2817aeef5966c9a6a0a442e37b963b8c84a199f6
Manuel Römer <30902964+manuelroemer@users.noreply.github.com>
2021-05-22 15:31:15 +0200

    Added helmet middleware.

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import passport from 'passport';
 import cors from 'cors';
+import helmet from 'helmet';
 import { establishDbConnection } from './db/connection';
 import { config } from './config';
 import { logger } from './log';
@@ -9,6 +10,7 @@ import { endpoints } from './endpoints/endpoints';
 import './middlewares/passport';
 
 const app = express();
+app.use(helmet());
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
