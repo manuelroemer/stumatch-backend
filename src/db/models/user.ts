aaebd8c56082a5563aa1fb00530255ac823dc3e4
n.duong <nhu.duong@tum.de>
2021-07-05 12:46:58 +0200

    fixes

--- src/db/models/user.ts
+++ src/db/models/user.ts
@@ -11,7 +11,7 @@ export interface User extends DbObject {
   lastName: string;
   facultyId?: string;
   studyProgramId?: string;
-  startingSemester?: string;
+  startingSemester?: 'WS' | 'SS';
   startingYear?: number;
   roles: Array<UserRole>;
 }
