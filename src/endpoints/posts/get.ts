222785dc62d32d7f9ac155ebb02dbbe504a47388
Jonas G <jonas.grossmann96@gmail.com>
2021-07-11 21:32:04 +0200

    Support features for changes in the FE

--- src/endpoints/posts/get.ts
+++ src/endpoints/posts/get.ts
@@ -1,5 +1,6 @@
 import { Router } from 'express';
 import { PostModel } from '../../db/models/post';
+import { UserModel } from '../../db/models/user';
 import { NotFoundError } from '../../dtos/apiErrors';
 import { apiResult } from '../../dtos/apiResults';
 import { authenticateJwt } from '../../middlewares/authenticateJwt';
@@ -12,8 +13,15 @@ const handler = asyncRequestHandler(async (req, res) => {
   if (!post) {
     throw new NotFoundError();
   }
+  const post_author = await UserModel.findById(post.authorId);
+  const post_w_a = {
+    ...post.toObject(),
+    author: post_author,
+    likes: 100,
+    comments: 100,
+  };
 
-  return res.status(200).json(apiResult(post.toObject()));
+  return res.status(200).json(apiResult(post_w_a));
 });
 
 export default Router().get('/api/v1/posts/:id', authenticateJwt, handler);
