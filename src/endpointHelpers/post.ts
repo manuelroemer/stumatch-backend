001d368b903ff072ddbd0ca74ea6191732c456a0
Jonas G <jonas.grossmann96@gmail.com>
2021-07-13 18:43:17 +0200

    Added likes Feature for posts

--- src/endpointHelpers/post.ts
+++ src/endpointHelpers/post.ts
@@ -1,13 +1,15 @@
+import { LikeModel } from '../db/models/Like';
 import { Post } from '../db/models/post';
 import { UserModel } from '../db/models/user';
 
 export async function getEnrichedPostDto(post: Post) {
   const author = await UserModel.findById(post.authorId);
+  const likes = await LikeModel.find({ postId: post.id });
 
   return {
     ...post,
     author,
-    likes: 100,
+    likes,
     comments: 100,
   };
 }
---
+++ src/endpoints/likes/delete.ts
@@ -0,0 +1,19 @@
+import { Router } from 'express';
+import { authenticateJwt } from '../../middlewares/authenticateJwt';
+import { asyncRequestHandler } from '../../utils/asyncRequestHandler';
+import { LikeModel } from '../../db/models/Like';
+import { NotFoundError } from '../../dtos/apiErrors';
+
+const handler = asyncRequestHandler(async (req, res) => {
+  const id = req.params.id;
+  const like = await LikeModel.findById(id);
+  if (!like) {
+    throw new NotFoundError();
+  }
+
+  await like.delete();
+
+  return res.sendStatus(204);
+});
+
+export default Router().delete('/api/v1/posts/likes/:id', authenticateJwt, handler);
