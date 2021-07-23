070740d1e4cf1a66f895de417413616dc971652a
Quoc Khang Phan <ga45wod@mytum.de>
2021-07-23 23:58:46 +0200

    fix getRandom

--- src/endpoints/feedAdvertisements/getRandom.ts
+++ src/endpoints/feedAdvertisements/getRandom.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express';
 import { AdvertisementModel } from '../../db/models/advertisement';
+import { NotFoundError } from '../../dtos/apiErrors';
 import { apiResult } from '../../dtos/apiResults';
 import { authenticateJwt } from '../../middlewares/authenticateJwt';
 import { asyncRequestHandler } from '../../utils/asyncRequestHandler';
 import { getUserOrThrow } from '../../utils/requestHelpers';
+import { getEnrichedAdvertisementDto } from '../../endpointHelpers/advertisement';
 
 const handler = asyncRequestHandler(async (req, res) => {
   const now = new Date();
@@ -13,11 +15,17 @@ const handler = asyncRequestHandler(async (req, res) => {
     status: 'verified',
     startDate: { $lte: now },
     endDate: { $gte: now },
-    $or: [{ facultyId: user.facultyId }, { facultyId: null }],
+    $or: [
+      { $and: [{ $or: [{ facultyId: user.facultyId }, { facultyId: null }] }, { studyProgramId: null }] },
+      { $and: [{ studyProgramId: user.studyProgramId }, { $not: { studyProgramId: null } }] },
+    ],
   };
 
   const aggregationResult = await AdvertisementModel.aggregate().match(query).sample(1);
-  return !aggregationResult ? res.status(200).json(undefined) : res.status(200).json(aggregationResult[0]);
+  if (!aggregationResult) {
+    throw new NotFoundError();
+  }
+  return res.status(200).json(apiResult(await getEnrichedAdvertisementDto(aggregationResult[0])));
 });
 
 export default Router().get('/api/v1/feedAdvertisements/random', authenticateJwt, handler);
