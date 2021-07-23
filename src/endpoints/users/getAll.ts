1b5ce4256e4f6c3b48e675cb19f3ac8721c6f12c
n.duong <nhu.duong@tum.de>
2021-07-23 19:02:52 +0200

    added filter for searching for users in Role management

--- src/endpoints/users/getAll.ts
+++ src/endpoints/users/getAll.ts
@@ -12,7 +12,13 @@ const sortableFields: Array<SortableFields<User>> = ['id', 'createdOn', 'modifie
 const handler = asyncRequestHandler(async (req, res) => {
   const thisUser = getUserOrThrow(req);
   const sort = getSortQueryFromUrl(req, sortableFields);
-  const paginationResult = await UserModel.paginate(getPaginationOptions(req), {}, undefined, { sort });
+  const filter = req.query.filter?.toString();
+
+  const query = filter
+    ? { $or: [{ firstName: { $regex: filter, $options: 'i' } }, { lastName: { $regex: filter, $options: 'i' } }] }
+    : undefined;
+
+  const paginationResult = await UserModel.paginate(getPaginationOptions(req), query, undefined, { sort });
   const result = paginationResult.docs.map((doc) => getEnrichedUserDto(doc.toObject(), thisUser));
   return res.status(200).json(paginationApiResult(result, paginationResult));
 });
