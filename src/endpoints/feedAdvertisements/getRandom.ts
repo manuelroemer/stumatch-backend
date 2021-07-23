7f0b1353739a3cd49fe8d7c5171cb34ab6cebca3
Quoc Khang Phan <ga45wod@mytum.de>
2021-07-23 20:15:58 +0200

    WIP getrandom

--- src/endpoints/feedAdvertisements/getRandom.ts
+++ src/endpoints/feedAdvertisements/getRandom.ts
@@ -3,10 +3,18 @@ import { AdvertisementModel } from '../../db/models/advertisement';
 import { apiResult } from '../../dtos/apiResults';
 import { authenticateJwt } from '../../middlewares/authenticateJwt';
 import { asyncRequestHandler } from '../../utils/asyncRequestHandler';
+import { getUserOrThrow } from '../../utils/requestHelpers';
 
 const handler = asyncRequestHandler(async (req, res) => {
   const now = new Date();
-  const query = { status: 'verified', startDate: { $lte: now }, endDate: { $gte: now } };
+  const user = getUserOrThrow(req);
+
+  const query = {
+    status: 'verified',
+    startDate: { $lte: now },
+    endDate: { $gte: now },
+    $or: [{ facultyId: user.facultyId }, { facultyId: null }],
+  };
 
   const aggregationResult = await AdvertisementModel.aggregate().match(query).sample(1);
   return !aggregationResult ? res.status(200).json(undefined) : res.status(200).json(aggregationResult[0]);
