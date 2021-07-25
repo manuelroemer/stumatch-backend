65b10d235c52b296e4ab54cd8eec56a22876b54b
Jonas G <jonas.grossmann96@gmail.com>
2021-07-25 13:30:56 +0200

    fixed seeding issues

--- src/db/models/commentSeed.ts
+++ src/db/models/commentSeed.ts
@@ -3,10 +3,10 @@ import faker from 'faker';
 import { Comment } from './comment';
 import { random } from 'lodash';
 
-export const commentSeed: Array<Comment> = range(faker.datatype.number(100)).map((i) => ({
+export const commentSeed: Array<Comment> = range(3000).map((i) => ({
   id: `10000000-0000-1000-8000-${i.toString().padStart(12, '0')}`,
   content: faker.lorem.sentences(random(2, 12)),
-  authorId: `00000000-0000-1000-8000-00000000000${faker.datatype.number(10).toString()}`,
+  authorId: `00000000-0000-1000-8000-00000000000${faker.datatype.number(8).toString()}`,
   postId: `10000000-0000-1000-8000-${faker.datatype.number(100).toString().padStart(12, '0')}`,
   createdOn: faker.date.past(),
 }));
