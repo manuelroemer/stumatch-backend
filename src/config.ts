a49de3e2e75cfa20c01ff5694b9688fe9690c327
Manuel Römer <30902964+manuelroemer@users.noreply.github.com>
2021-07-28 10:00:08 +0200

    Deployment config.

--- src/config.ts
+++ src/config.ts
@@ -8,7 +8,7 @@ if (process.env.NODE_ENV !== 'production') {
 }
 
 export const config = {
-  serverPort: process.env.SERVER_PORT ?? 4040,
+  serverPort: process.env.SERVER_PORT ?? process.env.$PORT ?? 4040,
   dbConnectionString: process.env.DB_CONNECTION_STRING ?? '',
   authJwtSecret: process.env.AUTH_JWT_SECRET ?? 'nosecret',
   authJwtIssuer: process.env.AUTH_JWT_ISSUER ?? 'sTUMatch',
